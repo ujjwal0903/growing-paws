7a454cb9f66c093909b1c5bb6e33c1e3ef89cd27
souravrjpt <souravrajput2760@gmail.com>
2023-12-02 17:43:01 +0530

    Created a seperate jsx file for all routes.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,26 +1,18 @@
 
-import Homepage from './body/sections/export-all';
-import SignIn from './pages/Sign-up-page';
-import Login from './pages/Login_page'
-import { Route, Routes } from 'react-router-dom';
+// import Homepage from './body/sections/export-all';
+// import SignIn from './pages/Sign-up-page';
+// import Login from './pages/Login_page'
+// import { Route, Routes } from 'react-router-dom';
+
+import React from "react";
+import Routing from './Routing'
 
 export default function App() {
   return (
     <>
       {/* ---- THIS IS FOR THE HOMEPAGE COMPONENTS ------*/}
 
-      <Routes>
-        {/* <Homepage/> */}
-        <Route path='/' element={<Homepage/>} />
-
-        {/* ----SIGN-IN PAGE COMPONENT----- */}
-        <Route path='signup' element={<SignIn/>} />
-        {/* <SignIn/> */}
-
-        {/* -----Login Page--------- */}
-          <Route path='login' element={<Login/>}/> 
-      </Routes>
-
+     <Routing/>
       
     </>
   );
---
+++ Frontend/src/Routing.jsx
@@ -0,0 +1,26 @@
+import Homepage from './body/sections/export-all';
+import SignIn from './pages/Sign-up-page';
+import Login from './pages/Login_page'
+import { Route, Routes } from 'react-router-dom';
+
+export default function Routing(){
+    return (
+        <>
+      {/* ---- THIS IS FOR THE HOMEPAGE COMPONENTS ------*/}
+
+      <Routes>
+        {/* <Homepage/> */}
+        <Route path='/' element={<Homepage/>} />
+
+        {/* ----SIGN-IN PAGE COMPONENT----- */}
+        <Route path='signup' element={<SignIn/>} />
+        {/* <SignIn/> */}
+
+        {/* -----Login Page--------- */}
+          <Route path='login' element={<Login/>}/> 
+      </Routes>
+
+      
+    </>
+    );
+}
