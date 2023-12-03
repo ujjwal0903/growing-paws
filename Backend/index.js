4669adf05b1c945dacee9f79d6e6f8aef8b4dcc1
souravrjpt <souravrajput2760@gmail.com>
2023-12-03 18:50:25 +0530

    connceted backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,19 @@
 const express=require('express')
 const app=express()
+const cors=require('cors')
 const path=require('path')
+const BodyParser=require('body-parser')
 // const signin=require('/Frontend/src/pages/sign-in.jsx')
 
+app.use(cors());
+app.use(BodyParser.json())
+
+
+app.post("/signup",(req,res)=>{
+    res.send(req.body)
+    console.log(req.body)
+
+})
 
 
 app.listen(5000,()=>{
