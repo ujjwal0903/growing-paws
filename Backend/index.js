b055743056edf743ae66ae179131b2b3994d37cf
souravrjpt <souravrajput2760@gmail.com>
2023-12-04 21:26:57 +0530

    Connected with mongodb, created schema

--- Backend/index.js
+++ Backend/index.js
@@ -3,17 +3,47 @@ const app=express()
 const cors=require('cors')
 const path=require('path')
 const BodyParser=require('body-parser')
+const mongoose=require('mongoose')
 // const signin=require('/Frontend/src/pages/sign-in.jsx')
+
+
+//------mongoDB connection-----------
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/project_db');
+  console.log("db connected")
+}
+
+const userSchema=new mongoose.Schema({
+    fullName:String,
+    userName:String,
+    Email:String,
+    password:String
+
+})
+
+const User = mongoose.model('User', userSchema);
+
+
 
 app.use(cors());
 app.use(BodyParser.json())
 
 
-app.post("/signup",(req,res)=>{
-    res.send(req.body)
-    console.log(req.body)
+app.post("/signup",async (req,res)=>{
+    let user=new User();
+    user.fullName=req.body.fullname;
+    user.userName=req.body.username;
+    user.Email=req.body.email;
+    user.password=req.body.password;
+
+    const doc= await user.save();
+    res.send(doc)
+    console.log(doc)
 
 })
+
 
 
 app.listen(5000,()=>{
